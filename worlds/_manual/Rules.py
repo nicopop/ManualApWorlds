496e27af258747a828e84882c1f2c0d6c4c83852
B-D-I1701 <b_d_i@iinet.net.au>
2023-04-20 15:17:16 +0800

    Add new format for "required" in locations.json
    this allows for "required": "item1 && (item2 || item3)" in locations.json making it easier and more readable to create logic

--- worlds/_manual/Rules.py
+++ worlds/_manual/Rules.py
@@ -3,6 +3,7 @@ from ..AutoWorld import World
 from BaseClasses import MultiWorld
 import re
 
+
 def infix_to_postfix(expr):
     prec = {"&": 2, "|": 2, "!": 3}
 
@@ -27,6 +28,7 @@ def infix_to_postfix(expr):
 
     return postfix
 
+
 def evaluate_postfix(expr):
     stack = []
     for c in expr:
@@ -47,48 +49,90 @@ def evaluate_postfix(expr):
             stack.append(not op)
     return stack.pop()
 
+
 def set_rules(base: World, world: MultiWorld, player: int):
     # Location access rules
     for location in base.location_table:
         locFromWorld = world.get_location(location["name"], player)
-        if "requires" in location: # Specific item access required
-            def fullLocationCheck(state, location=location):
-                # parse user written statement into list of each item
-                reqires_raw = re.split('(\[|&&|\|\||]| AND | OR)', location["requires"]) # regex probaly misses some cases i have to relearn regex everytime i use it
-                reqires_stripped = [x.strip() for x in reqires_raw]
-                requires_list = [x for x in reqires_stripped if x != '']
-                
-                for i, item in enumerate(requires_list):
-                    if item == "[":                     # square brackets used so user can name items with ()
-                        requires_list[i] = "("
-                    elif item == "]":
-                        requires_list[i] = ")"
-                    elif item == "||" or item == "OR":
-                        requires_list[i] = "|"
-                    elif item == "&&" or item == "AND":
-                        requires_list[i] = "&"
-                    else:
-                        item_parts = item.split(":")
-                        item_name = item
-                        item_count = 1
+        if "requires" in location:  # Specific item access required
+            # item access is in string logic form
+            if isinstance(location["requires"], str):
 
-                        if len(item_parts) > 1:
-                            item_name = item_parts[0]
-                            item_count = int(item_parts[1])
+                def fullLocationCheckString(state, location=location):
+                    # parse user written statement into list of each item
+                    # regex probaly misses some cases i have to relearn regex everytime i use it
+                    reqires_raw = re.split('(\&&|\|\||\)|\(| AND | OR )', location["requires"])
+                    reqires_stripped = [x.strip() for x in reqires_raw]
+                    requires_list = [x for x in reqires_stripped if x != '']
 
-                        if state.has(item_name, player, item_count):    # if player has the item replace the text with 1 otherwiwse 0
-                            requires_list[i] = "1"
+                    for i, item in enumerate(requires_list):
+                        if item == "||" or item == "OR":
+                            requires_list[i] = "|"
+                        elif item == "&&" or item == "AND":
+                            requires_list[i] = "&"
                         else:
-                            requires_list[i] = "0"
+                            item_parts = item.split(":")
+                            item_name = item
+                            item_count = 1
 
-                requires_string = infix_to_postfix("".join(requires_list))
-                return(evaluate_postfix(requires_string))
+                            if len(item_parts) > 1:
+                                item_name = item_parts[0]
+                                item_count = int(item_parts[1])
 
-            set_rule(locFromWorld, fullLocationCheck)
-        else: # Only region access required
+                            if state.has(item_name, player, item_count):
+                                requires_list[i] = "1"
+                            else:
+                                requires_list[i] = "0"
+
+                    requires_string = infix_to_postfix("".join(requires_list))
+                    return (evaluate_postfix(requires_string))
+
+                set_rule(locFromWorld, fullLocationCheckString)
+
+            else:  # item access is in dict form
+
+                def fullLocationCheck(state, location=location):
+                    canAccess = True
+
+                    for item in location["requires"]:
+                        if isinstance(item, dict) and "or" in item and isinstance(item["or"], list):
+                            canAccessOr = True
+
+                            for or_item in item["or"]:
+                                or_item_parts = or_item.split(":")
+                                or_item_name = or_item
+                                or_item_count = 1
+
+                                if len(or_item_parts) > 1:
+                                    or_item_name = or_item_parts[0]
+                                    or_item_count = int(or_item_parts[1])
+
+                                if not state.has(or_item_name, player, or_item_count):
+                                    canAccessOr = False
+
+                            if canAccessOr:
+                                canAccess = True
+                                break
+                        else:
+                            item_parts = item.split(":")
+                            item_name = item
+                            item_count = 1
+
+                            if len(item_parts) > 1:
+                                item_name = item_parts[0]
+                                item_count = int(item_parts[1])
+
+                            if not state.has(item_name, player, item_count):
+                                canAccess = False
+
+                    return canAccess
+                set_rule(locFromWorld, fullLocationCheck)
+
+        else:  # Only region access required
             def allRegionsAccessible(state, location=location):
                 return True
-            set_rule(locFromWorld, allRegionsAccessible) # everything is in the same region in manual
+            # everything is in the same region in manual
+            set_rule(locFromWorld, allRegionsAccessible)
 
     # Victory requirement
     world.completion_condition[player] = lambda state: state.has("__Victory__", player)
