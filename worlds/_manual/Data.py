63734e73c4400e9977efedb7ad6afae40849be79
Fuzzy <slightlyfuzzybeard@gmail.com>
2023-05-04 12:40:51 -0500

    Add validation for common typos and mistakes

--- worlds/_manual/Data.py
+++ worlds/_manual/Data.py
@@ -1,6 +1,9 @@
 import json
 import os
 import pkgutil
+import sys
+
+from .DataValidation import DataValidation
 
 # blatantly copied from the minecraft ap world because why not
 def load_data_file(*args) -> dict:
@@ -19,3 +22,32 @@ item_table = load_data_file('items.json')
 progressive_item_table = {}
 location_table = load_data_file('locations.json')
 region_table = load_data_file('regions.json')
+
+# seed all of the tables for validation
+DataValidation.game_table = game_table
+DataValidation.item_table = item_table
+DataValidation.location_table = location_table
+DataValidation.region_table = region_table
+
+# show the apworld creator what their game name and filename should be
+DataValidation.outputExpectedGameNames()
+
+try:
+    # check that requires have correct item names in locations and regions
+    DataValidation.checkItemNamesInLocationRequires()
+    DataValidation.checkItemNamesInRegionRequires()
+
+    # check that region names are correct in locations
+    DataValidation.checkRegionNamesInLocations()
+
+    # check that items that are required by locations and regions are also marked required
+    DataValidation.checkItemsThatShouldBeRequired()
+
+    # check that regions that are connected to are correct
+    DataValidation.checkRegionsConnectingToOtherRegions()
+
+    # check that the apworld creator didn't specify multiple victory conditions
+    DataValidation.checkForMultipleVictoryLocations()
+except Exception as e:
+    sys.exit("ValidationError: %s" % (e))
+    
