40eebb1260357d8e768617ce4369679f2ce20b80
nicopop <6759630+nicopop@users.noreply.github.com>
2024-04-23 19:33:13 -0400

    small hack so test work (maybe)

--- BaseClasses.py
+++ BaseClasses.py
@@ -1025,6 +1025,10 @@ class Location:
         return self.item is not None and self.item.advancement
 
     @property
+    def event(self) -> bool:
+        return self.advancement
+
+    @property
     def is_event(self) -> bool:
         """Returns True if the address of this location is None, denoting it is an Event Location."""
         return self.address is None
