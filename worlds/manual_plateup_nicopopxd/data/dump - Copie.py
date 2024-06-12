606e9a073327040d5693d04b6742277a74e58897
nicopop <6759630+nicopop@users.noreply.github.com>
2024-06-12 19:37:46 -0400

    files cleanup

--- worlds/manual_plateup_nicopopxd/data/dump - Copie.py
+++
@@ -1,6 +0,0 @@
-import json
-for file in ["items.json", "locations.json"]:
-    with open(file, 'r') as ifile:
-        data = json.load(ifile)
-    with open(file+"Names", 'w') as ofile:
-        json.dump([i['name'] for i in data], ofile)
